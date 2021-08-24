a448ed76f12165a7689afe3c948ba2b666a603a5
roxanne lessard <me@rlessard.com>
2021-08-24 13:11:38 +0100

    added the controllers and routes

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,23 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,24 @@
+class ProfilesController < ApplicationController
+
+  def index
+    @profiles = Profile.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
