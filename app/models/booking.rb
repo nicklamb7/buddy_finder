abe65b97d56ad9aad6e28a70f11e666ec3f50311
roxanne lessard <me@rlessard.com>
2021-08-24 16:30:56 +0100

    db:seed minimum data done and add dependecies for destroy in the models + add a method in booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,20 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :profile
 
-  validates :start_date, presence: true, comparison: { greater_than: :end_date }
+  validates :start_date, presence: true
   validates :end_date, presence: true
+  # validates :start_date, presence: true, comparison: { less_than: :end_date }
+  # validates :start_date, presence: true, comparison: { greater_than: :end_date }
   validates :event_location, presence: true
   validates :event_name, presence: true, length: { minimum: 2 }
+
+  validate :end_date_after_start_date?
+
+  private
+
+  def end_date_after_start_date?
+    if end_date < start_date
+      errors.add :end_date, "must be after start date"
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  puts "Destroying users..."
+  User.destroy_all
+  puts "Destroying profiles..."
+  Profile.destroy_all
+  puts "Destroying bookings..."
+  Booking.destroy_all
+end
+
+puts "create user"
+user_a = User.create!(email: "nick@gmail.com", password: "hwhlaaqeor")
+user_b = User.create!(email: "me@rlessard.com", password: "hidceghgwj")
+
+puts "create profile"
+profile_a = Profile.create!(user: user_a, first_name: "alex", last_name: "natalia", bio: "Wonder Woman 1984", interests: "Wonder, Woman, Soviet Union, Cold War", picture: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", location: "hoxton")
+profile_b = Profile.create!(user: user_b, first_name: "john", last_name: "christina", bio: "The Shawshank Redemption", interests: "double murder, banker, new life, prison", picture: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", location: "london")
+
+puts "create booking"
+start_date = Date.new(2021, 2, 5)
+end_date = Date.new(2021, 2, 6)
+booking_a = Booking.create(user: user_a, profile: profile_a, event_name: "Titanic", event_location: "canada", start_date: start_date, end_date: end_date)
+puts "Everything created!!!"
